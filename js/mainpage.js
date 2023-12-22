0a41e9de98b2a62aaec8fc522604990b1f4728b5
soheil babaee <soheilbabaee021@gmail.com>
2023-12-22 13:50:58 +0330

    The input validation was done and it works by pressing the enter button

--- js/mainpage.js
+++ js/mainpage.js
@@ -45,43 +45,6 @@ $("#submit-id").click(function(event) {
   event.preventDefault();
   submitForm();
 });
-
-
-
-
-
-
-// setTimeout( function() {
-//   $("#div-gray").show();
-//   $("#input-name-div-id").show();
-// }, 100);
-// $("#submit-id").click(function(i){ 
-// i.preventDefault();
-// let fullname = $("#input-text-name");
-// $("#showname").html(fullname.val());
-
-// if(fullname.val().length > 2){
-//   $("#input-name-div-id").hide();
-//   $(".div_loading").show();
-//   setTimeout( function() {
-//     $(".div_loading").hide();
-
-//   }, 4000);
-//   setTimeout(function() {
-//     $("#input-name-div-id-ver1").show();
-//   }, 4001);
-//   setTimeout(function()  {
-//     $("#div-gray").fadeOut(600);
-//     $("#input-name-div-id").fadeOut(600);
-//     $(".div_loading").fadeOut(600);
-//     $("#input-name-div-id-ver1").hide();
-//     document.getElementById("welcome-message").classList.remove("welcome-message-div-blur");
-//     }, 7000);
-//    }else{
-//     $(".alert-p-showname").show();
-//    }
-  
-//   });
 // end Code JS for message input name
 
 
