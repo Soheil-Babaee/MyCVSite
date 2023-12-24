d0812567147bf4c4f62af5d93b3ab8aa95e7914b
soheil babaee <soheilbabaee021@gmail.com>
2023-12-24 19:09:16 +0330

    Almost all files have been updated

--- js/contactus.js
+++ js/contactus.js
@@ -76,7 +76,7 @@
                 $(".gray-div").show();
                      $("#loading-div-id").show();
                           setTimeout(function(){
-                             $("#loading-div-id").hide();
+                             $("#loading-div-id").hide("fast");
                               $("#loading-div-id-ver1").show();
                          },4000)
             }else{
@@ -90,33 +90,8 @@
      });
 
      $("#close-btn").click(function(){
-             $("#loading-div-id-ver1").hide();
-         $("#loading-div-id").hide();
-         $(".gray-div").hide();
+             $("#loading-div-id-ver1").hide("fast");
+         $("#loading-div-id").hide("fast");
+         $(".gray-div").fadeOut(600);
          $("#showalert").hide();
          });
-
-// $(".btn-send").click(function(i){
-//     let all_input = $("form input");
-//     let txt_area = $(" form textarea");
-//     i.preventDefault();
-//     if ( all_input.val().length > 2 && txt_area.val().length > 5 ) {
-//         $("#showalert").hide();
-//          $(".gray-div").show();
-//        $("#loading-div-id").show();
-//          setTimeout(function(){
-//             $("#loading-div-id").hide();
-//              $("#loading-div-id-ver1").show();
-//         },4000)
-
-//     }else{
-//         $("#showalert").show();
-//         return false;
-//     }
-// });
-// $("#close-btn").click(function(){
-//     $("#loading-div-id-ver1").hide();
-// $("#loading-div-id").hide();
-// $(".gray-div").hide();
-// $("#showalert").hide();
-// });
--- js/mainpage.js
+++ js/mainpage.js
@@ -1,10 +1,9 @@
 // Start Code JS for message input name
-setTimeout( function() {
-     $("#div-gray").show();
-     $("#input-name-div-id").show();
-   }, 100); 
-
-$("body").keypress(function(ascii_code) {
+setTimeout(function () {
+  $("#div-gray").show();
+  $("#input-name-div-id").show();
+}, 100);
+$("body").keypress(function (ascii_code) {
   if (ascii_code.which === 13) {
       ascii_code.preventDefault();
       submitForm();
@@ -13,21 +12,31 @@ $("body").keypress(function(ascii_code) {
 
 function submitForm() {
   let fullname = $("#input-text-name");
-  $("#showname").html(fullname.val());
+   $("#showname").html(fullname.val());
 
   if (fullname.val().length > 2) {
+      var isValid = /^[\u0600-\u06FF\s]+$/.test(fullname.val());
+      if (!isValid) {
+        $(".alert-p-charecter").show("fast");
+        setTimeout(function()  {
+          $(".alert-p-charecter").hide("fast");
+    
+        }, 5000);
+          return;
+      }
+
       $("#input-name-div-id").hide("fast");
       $(".div_loading").show();
-      
-      setTimeout(function() {
+
+      setTimeout(function () {
           $(".div_loading").hide("fast");
       }, 4000);
 
-      setTimeout(function() {
+      setTimeout(function () {
           $("#input-name-div-id-ver1").show();
       }, 4001);
 
-      setTimeout(function() {
+      setTimeout(function () {
           $("#div-gray").fadeOut(600);
           $("#input-name-div-id").fadeOut(600);
           $(".div_loading").fadeOut(600);
@@ -36,12 +45,13 @@ function submitForm() {
       }, 7000);
   } else {
       $(".alert-p-showname").show("fast");
-      setTimeout( function() {
-        $(".alert-p-showname").hide("fast");
-      }, 3000);
+      setTimeout(function () {
+          $(".alert-p-showname").hide("fast");
+      }, 5000);
   }
 }
-$("#submit-id").click(function(event) {
+
+$("#submit-id").click(function (event) {
   event.preventDefault();
   submitForm();
 });
